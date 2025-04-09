edb0f7b7f604d820c240bfb3c34fb4482a4e949e
Kapish <KeapeR@Kapishs-MacBook-Pro.local>
2025-04-09 10:19:42 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './components/navbar/Navbar.jsx'
+
+
+function App() {
+
+  return (
+    <>
+      <header><Navbar /></header>
+      <main>
+        <section className=''></section>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-center items-center text-gray-900'>
+      <i class="bi bi-graph-up-arrow"></i>
+    </nav>
+  )
+}
+
+export default Navbar
